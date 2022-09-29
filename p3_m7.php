95ecd6d21c7c66f84ec17eab8e4f4999c47412e9
hvega25 <hvega@jaumebalmes.net>
2022-09-29 20:24:08 +0200

    segunda version por herson

--- p3_m7.php
+++ p3_m7.php
@@ -1,1 +1,35 @@
-#php 
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class vehicle {
+
+    //atributos
+    public $marca;
+    public $modelo;
+    public $year;
+    public $color;
+    public $precio;
+
+    function __construct() {
+    }
+
+    function __construct1($marca, $precio) {
+        $this->marca = $marca;
+        $this->precio = $precio;
+    }
+
+    function __construct2($marca, $modelo, $year, $color, $precio) {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->year = $year;
+        $this->color = $color;
+        $this->precio = $precio;
+    }
+
+}
+?>
+
+</body>
+</html>
