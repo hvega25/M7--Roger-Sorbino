4f587ad017c5e9df83f12b5bef909139f5646c0f
4Tipy <lchavez21@jaumebalmes.net>
2022-10-13 20:37:41 +0200

    mis cambios :V

---
+++ php_mysql/index2.php
@@ -0,0 +1,41 @@
+<?php
+$db_host = "localhost";
+$db_nombre = "products";
+$db_usario = "root";
+$db_passwd = "";
+
+$connexio = mysqli_connect($db_host, $db_usario, $db_passwd, $db_nombre);
+$consulta = "Select * FROM PRODUCTS";
+$products = mysqli_query($connexio, $consulta);
+
+
+$name = $_GET["name"] ;
+$description = $_GET["description"] ;
+$price = $_GET["price"];
+$instruccion_SQL = "INSERT INTO PRODUCTS (name, productos,  price) VALUES ('$name','$description','$price')";
+$resultado = mysqli_query($connexio,$instruccion_SQL);
+
+?>
+<link rel="stylesheet" href="estilos.css">
+<body>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">name</th>
+        <th scope="col">description</th>
+        <th scope="col">price</th>
+    </tr>
+    </thead>
+    <?php foreach ($products as $i => $productos) { ?>
+        <tr>
+            <th scope="row"><?php echo $i+ 1?></th>
+            <td><?php echo $productos['name'] ?></td>
+            <td><?php echo $productos['description'] ?></td>
+            <td><?php echo $productos['price'] ?></td>
+            <td><button type="button" class="btn btn-outline-primary">Edit</button> </td>
+            <td><button type="button" class="btn btn-outline-danger">Delete</button> </td>
+        </tr>
+    <?php } ?>
+</table>
+</body>
